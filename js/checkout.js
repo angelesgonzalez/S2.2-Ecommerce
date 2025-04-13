ab90f1cce4093381725da2252c3d3d01f91f3d3d
angelesgonzalez <delosangelesgonzalez@gmail.com>
2025-04-13 13:33:44 +0200

    Create addEventlistener to check the form upon sumbit

--- js/checkout.js
+++ js/checkout.js
@@ -1,50 +1,25 @@
-/* Hasta este ejercicio solo hemos implementado la lógica de la pantalla principal de la tienda. Ahora implementaremos la validación del formulario de checkout que se encuentra en el archivo checkout.js.
-
-Para acceder a esta pantalla, debes hacer clic en el icono del carrito en la parte superior derecha de la pantalla, apareciendo un modal con el botón que te llevará a dicha pantalla.
- Tu primer objetivo será validar el formulario checkout.html 
-utilizando el archivo checkout.js.
-En este ejercicio deberás implementar la lógica para que los campos del formulario cumplan las siguientes condiciones:
-
-
-!1- Cargar el form id = checkout-form
-add eventlistener onchange 
-!2- add eventlistener to the form
-!2- if(at least one is not filled) - return
-!3- if (at least one of the inputs don't have 3 caracters ) - return
-!4- validationName - LastName -> only letters
-!5- Password needs to have at least 4 carachters + one letter & one number
-!6- email needs to have email format. 
-!Invalid phone number!! Must be 9 digits with no
-!when input is invalid, add is-invalid class to the element 
-!password (4-8 characters)"
-
-
-							
-
- * Todos los campos son obligatorios.
- * Todos los campos deben tener al menos 3 caracteres.
-* El nombre y los apellidos deben contener solo letras.
- * El teléfono debe contener solo números.
- * La contraseña debe incluir números y letras.
- * El email debe tener formato de correo electrónico.
- *Cuando el/la usuario/a introduzca un campo que no cumpla con las validaciones anteriores, el input debe resaltarse en rojo y mostrar un mensaje en la parte inferior.
-*Ayuda: podrás colorear el borde del input en rojo y mostrar el mensaje de error manipulando el DOM, aunque también puedes usar la clase is-invalid de Bootstrap.
-
-1- required already in html
-2- every input needs to check that is not null === '' 
-3- Name & LastName only letters, not null
-
-
-
-
-*/
-
 document.addEventListener("DOMContentLoaded", (event) => {
 	const inputs = document.querySelectorAll("input");
+	const form = document.getElementById("checkout-form");
 
 	inputs.forEach((input) => {
 		input.addEventListener("change", (event) => {
 			validateField(event.target);
+		});
+	});
+
+	form.addEventListener("submit", (event) => {
+		event.preventDefault();
+		let hasError = false;
+		//Review globally if there's an error before sending the form.
+		inputs.forEach((input) => {
+			const error = getValidations(input);
+			if (error) {
+				showError(input, error);
+				hasError = true;
+			} else {
+				input.classList.remove("is-invalid");
+			}
 		});
 	});
 });
@@ -62,35 +37,39 @@ const validateField = (element) => {
 const getValidations = (element) => {
 	const value = element.value.trim();
 
-	let errorMessage = "";
-
-	if (value === "" || value.length < 3) {
-		return (errorMessage = `Please insert at least 3 characters.`);
+	if (!["fPassword", "fPhone"].includes(element.id) && value.length < 3) {
+		return `Please insert at least 3 characters.`;
 	}
 
 	if (element.id === "fLastN" || element.id === "fName") {
 		if (!hasOnlyLetters(value)) {
-			return (errorMessage = `Please use letters only. Numbers and special characters are not allowed.`);
+			return `Please use letters only. Numbers and special characters are not allowed.`;
 		}
 	}
 
 	if (element.id === "fEmail") {
 		if (!isValidEmail(value)) {
-			return (errorMessage = `Please enter a valid email address. Example: name@example.com`);
+			return `Please enter a valid email address. Example: name@example.com`;
 		}
 	}
 
 	if (element.id === "fPassword") {
 		if (value.length < 4) {
-			return (errorMessage = `Password needs to be at least 4 characters long.`);
+			return `Password needs to be at least 4 characters long.`;
 		}
 
 		if (!isValidPassword(value)) {
-			return (errorMessage = `Password needs to include at least one letter and one number.`);
+			return `Password needs to include at least one letter and one number.`;
 		}
 	}
 
-	return errorMessage;
+	if (element.id === "fPhone") {
+		if (!isValidPhone(value)) {
+			return `Invalid phone number. Must be 9 digits with no letters`;
+		}
+	}
+
+	return "";
 };
 
 const hasOnlyLetters = (string) => {
@@ -107,8 +86,11 @@ const isValidEmail = (email) => {
 const isValidPassword = (password) => {
 	const hasLetter = /[A-Za-z]/.test(password);
 	const hasNumber = /\d/.test(password);
-
 	return hasLetter && hasNumber;
+};
+
+const isValidPhone = (phoneNumber) => {
+	return /^\d{9}$/.test(phoneNumber);
 };
 
 const showError = (element, error) => {
@@ -116,4 +98,3 @@ const showError = (element, error) => {
 	errorDiv.textContent = error;
 	element.classList.add("is-invalid");
 };
-
